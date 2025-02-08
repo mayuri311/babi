e1b188093c30e42b610595b92b9e5ea047c47776
mayuri311 <mayuri.murugs@gmail.com>
2025-02-08 18:21:38 -0500

    audio

--- frontend/main_auxilary.py
+++ frontend/main_auxilary.py
@@ -6,6 +6,18 @@ from contextlib import asynccontextmanager
 from nicegui import Client, app, core, run, ui
 from nicegui.events import ValueChangeEventArguments
 from datetime import datetime
+
+import sys
+import threading
+import time
+# import pyaudio
+import wave
+import cv2
+import os
+from fastapi import Response, status
+
+# from audio import live_segmentation as ap
+# import threadedAudioProcessing as tap
 # from webcam_detection import detect_boxes
 
 
@@ -36,7 +48,7 @@ def thereallyjankfunction():
     
     @app.get('/cryinglog')
     async def cryinglog():
-        with open("cryinglog.txt", "r") as f:
+        with open("/frontend/cryinglog.txt", "r") as f:
             lines = f.readlines()
         return lines
         
@@ -46,4 +58,63 @@ def thereallyjankfunction():
     # Release the webcam hardware so it can be used by other applications again.
         video_capture.release()
     
+
 thereallyjankfunction()
+
+# stop_threads = False
+# audio_segment_length = 10
+
+
+# # Audio sampling task
+# def audio_sampling():
+#     global stop_threads
+#     global audio_segment_length
+#     ap.segmentMake(audio_segment_length, lambda: stop_threads)
+
+# # Audio processing task (example: simple print statement for now)
+# def audio_processing():
+#     global stop_threads
+#     global audio_segment_length
+
+#     wav_path = os.path.join(os.getcwd(), "audio/Segments")
+
+#     while not stop_threads:
+#         # Simulate audio processing (e.g., analyzing the saved wav file)
+
+#         tap.processTopAudioFile(wav_path)
+#         print("Processing audio...")
+
+#         time.sleep(1)  # Placeholder for processing time
+
+# # Image processing task
+# def image_processing():
+#     global stop_threads
+
+#     thereallyjankfunction()
+
+# # Create threads for each task
+# audio_sampling_thread = threading.Thread(target=audio_sampling)
+# audio_processing_thread = threading.Thread(target=audio_processing)
+# image_processing_thread = threading.Thread(target=image_processing)
+
+# # Start the threads
+# audio_sampling_thread.start()
+# audio_processing_thread.start()
+# image_processing_thread.start()
+
+# try:
+#     # Main loop to keep the program running until interrupted
+#     while True:
+#         time.sleep(1)
+#         print(stop_threads)
+# except KeyboardInterrupt:
+#     # When Ctrl+C is pressed, set the stop flag
+#     stop_threads = True
+#     print("Stopping all tasks...")
+
+# # Wait for all threads to finish
+# audio_sampling_thread.join()
+# audio_processing_thread.join()
+# image_processing_thread.join()
+
+# print("Tasks quit successfully")
